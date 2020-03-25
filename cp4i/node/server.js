f5ef56da60f68764b2038662cf2eca3f4216dd9a
Pallavi <mitra.pallavi1@tcs.com>
2020-03-25 19:10:59 +0530

    CORS 4

--- cp4i/node/server.js
+++ cp4i/node/server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 //enable CORS
 app.use(cors({credentials: true, origin: UI_BASE_URL}));
 
-app.use(cors());
+//app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
