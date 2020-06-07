7f28afdeadf5d47f13137a09e4fec8937b50f8ab
Akhil Hada <akhil.hada@tcs.com>
2020-06-07 10:11:17 +0000

    Update environment.prod.ts updated api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiUrl: 'https://cp4i-prtl-backend.mybluemix.net/api/jumpstat',
+  //apiUrl: 'https://cp4i-prtl-backend.mybluemix.net/api/jumpstat',
+  apiUrl: 'https://cp4i-jumpstart-backend.eu-gb.mybluemix.net/api/jumpstat',
   production: true
 };
