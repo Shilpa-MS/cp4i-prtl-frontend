080e82e6c44d7b4d33b2616ff66ee0fecea35a6b
Akhil Hada <akhil.hada@tcs.com>
2020-06-07 10:12:07 +0000

    Update environment.staging.ts updated api url

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiUrl: 'https://cp4i-jumpstart-backend-test.eu-gb.mybluemix.net/api/jumpstat',
+  //apiUrl: 'https://cp4i-jumpstart-backend-test.eu-gb.mybluemix.net/api/jumpstat',
+  apiUrl: 'https://cp4i-jumpstart-backend.eu-gb.mybluemix.net/api/jumpstat',
   production: false
 };
